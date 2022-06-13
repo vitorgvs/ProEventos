931daaff5268ff477ed9004bf564786bdf1e4421
vitor <vitorvilarsilva@gmail.com>
2022-06-13 17:04:35 -0300

    Seção 4:Angular

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -11,12 +11,12 @@ namespace ProEventos.API.Controller
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class EventoController : ControllerBase
+    public class EventosController : ControllerBase
     {
             
         private readonly DataContext _context;
 
-        public EventoController(DataContext context)
+        public EventosController(DataContext context)
         {
             _context = context;
         }  
